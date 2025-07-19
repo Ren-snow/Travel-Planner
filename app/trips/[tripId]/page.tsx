34fbcd417972af53337e24bdb9aa820f20af6b5e
Ren <b.b.r.12.11@gmail.com>
2025-07-19 13:46:36 +0900

    Implement drag and drop functionality

--- app/trips/[tripId]/page.tsx
+++ app/trips/[tripId]/page.tsx
@@ -7,16 +7,18 @@ export default async function TripDetail({
 }: {
     params: Promise<{ tripId: string }>;
 }) {
-  const {tripId} =await params
-  const session = await auth()
-  if(!session) {
-    return <div>Please sign in.</div>
-  }
-  const trip = await prisma.trip.findFirst({
-    where: {id: tripId, userId: session.user?.id}
-  })
-  if(!trip) {
-    return <div>Trip not found.</div>
-  }
-  return <TripDetailClient trip={trip} />
+    const { tripId } = await params;
+    const session = await auth();
+    if (!session) {
+        return <div>Please sign in.</div>;
+    }
+    const trip = await prisma.trip.findFirst({
+        where: { id: tripId, userId: session.user?.id },
+        include: { locations: true },
+    });
+
+    if (!trip) {
+        return <div>Trip not found.</div>;
+    }
+    return <TripDetailClient trip={trip} />;
 }
--- components/trip-detail.tsx
+++ components/trip-detail.tsx
@@ -1,16 +1,21 @@
 "use client";
 
-import { Trip } from "@/app/generated/prisma";
+import { Location, Trip } from "@/app/generated/prisma";
 import Image from "next/image";
-import { Calendar, Plus } from "lucide-react";
+import { Calendar, MapPin, Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
 import { useState } from "react";
 import { TabsContent } from "@radix-ui/react-tabs";
+import Map from "@/components/map";
+import SortableItinerary from "./sortable-itinerary";
 
+export type TripWithLocation = Trip & {
+    locations: Location[];
+};
 interface TripDetailClientProps {
-    trip: Trip;
+    trip: TripWithLocation;
 }
 
 export default function TripDetailClient({ trip }: TripDetailClientProps) {
@@ -84,19 +89,98 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
                                                 <br />
                                                 {`${Math.round(
                                                     (trip.endDate.getTime() -
-                                                        trip.startDate.getTime()) / (1000 * 60 * 60 * 24)
+                                                        trip.startDate.getTime()) /
+                                                        (1000 * 60 * 60 * 24)
                                                 )} days`}
                                             </p>
                                         </div>
                                     </div>
                                     <div className="flex items-start">
-                                        
+                                        <MapPin className="h-6 w-6 mr-3 text-gray-500" />
+                                        <div>
+                                            <p>Destinations</p>
+                                            <p>
+                                                {trip.locations.length}{" "}
+                                                {trip.locations.length === 1
+                                                    ? "location"
+                                                    : "locations"}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div className="h-72 rounded-lg overflow-hidden shadow">
+                                <Map itineraries={trip.locations} />
+                            </div>
+                            {trip.locations.length === 0 && (
+                                <div className="text-center p-4">
+                                    <p>Add locations to see them on the map.</p>
+                                    <Link
+                                        href={`/trips/${trip.id}/itinerary/new`}
+                                    >
+                                        <Button>
+                                            <Plus className="mr-2 h-5 w-5" />
+                                            Add Location
+                                        </Button>
+                                    </Link>
+                                </div>
+                            )}
+                            <div>
+                                <p className="text-gray600 leading-relaxed">
+                                    {trip.description}
+                                </p>
+                            </div>
                         </div>
                     </TabsContent>
+                    <TabsContent value="itinerary" className="space-y-6">
+                        <div className="flex justify-between items-center mb-4">
+                            <h2 className="text-2xl font-semibold">
+                                Full Itinerary
+                            </h2>
+                        </div>
+                        {trip.locations.length === 0 ? (
+                            <div className="text-center p-4">
+                                <p>
+                                    Add locations to see them on the itinerary.
+                                </p>
+                                <Link href={`/trips/${trip.id}/itinerary/new`}>
+                                    <Button>
+                                        <Plus className="mr-2 h-5 w-5" />
+                                        Add Location
+                                    </Button>
+                                </Link>
+                            </div>
+                        ) : (
+                            <SortableItinerary
+                                locations={trip.locations}
+                                tripId={trip.id}
+                            />
+                        )}
+                    </TabsContent>
+                    <TabsContent value="map" className="space-y-6">
+                        <div className="h-72 rounded-lg overflow-hidden shadow">
+                            <Map itineraries={trip.locations} />
+                        </div>
+                        {trip.locations.length === 0 && (
+                            <div className="text-center p-4">
+                                <p>Add locations to see them on the map.</p>
+                                <Link href={`/trips/${trip.id}/itinerary/new`}>
+                                    <Button>
+                                        <Plus className="mr-2 h-5 w-5" />
+                                        Add Location
+                                    </Button>
+                                </Link>
+                            </div>
+                        )}
+                    </TabsContent>
                 </Tabs>
+            </div>
+            <div className="text-center">
+                <Link href={`/trips`}>
+                    <Button>
+                        Back to Trips
+                    </Button>
+                </Link>
             </div>
         </div>
     );
